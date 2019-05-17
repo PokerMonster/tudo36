5c0bd839f31b666c2181d3930ac99009db7235f9
Too <tootz@qq.com>
2019-05-17 21:33:53 +0800

    保存缩略图路径到数据库，并展示在 explore 页面

--- handlers/main.py
+++ handlers/main.py
@@ -25,10 +25,11 @@ class ExploreHandler(tornado.web.RequestHandler):
     最近上传的缩略图片页面
     """
     def get(self):
-        self.render('explore.html', a=100)
+        posts = get_all_posts()
+        self.render('explore.html', posts=posts)
 
 
-class PostHanlder(tornado.web.RequestHandler):
+class PostHanlder(BaseHandler):
     """
     单个图片详情页面
     """
@@ -53,6 +54,8 @@ class UploadHandler(BaseHandler):
             up_img = UploadImage(p['filename'], self.settings['static_path'])
             up_img.save_upload(p['body'])
             up_img.make_thumb()
-            post_id = add_post(up_img.image_url, self.current_user)
+            post_id = add_post(up_img.image_url,
+                               up_img.thumb_url,
+                               self.current_user)
 
         self.redirect('/post/{}'.format(post_id))
